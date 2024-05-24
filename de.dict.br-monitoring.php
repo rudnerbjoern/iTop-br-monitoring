dd7f861d770c301c12585717ee3f8a742c2c5925
Bjoern Rudner <rudnerbjoern@hotmail.com>
2024-05-24 11:45:23 +0200

    revert changes

---
+++ de.dict.br-monitoring.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * @copyright   Copyright (C) 2021-2023 Björn Rudner
+ * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
+ * @version     2023-02-13
+ *
+ * Localized data
+ */
+
+Dict::Add('DE DE', 'German', 'Deutsch', array(
+    // Dictionary entries go here
+    'Class:FunctionalCI/Attribute:monitoringstatus' => 'Monitoring Status',
+    'Class:FunctionalCI/Attribute:monitoringstatus+' => 'Status der Implementierung der Überwachung',
+    'Class:FunctionalCI/Attribute:monitoringstatus/Value:unknown' => 'unbekannt',
+    'Class:FunctionalCI/Attribute:monitoringstatus/Value:unknown+' => 'unbekannt',
+    'Class:FunctionalCI/Attribute:monitoringstatus/Value:implemented' => 'implementiert',
+    'Class:FunctionalCI/Attribute:monitoringstatus/Value:implemented+' => 'Eine Überwachung ist implementiert',
+    'Class:FunctionalCI/Attribute:monitoringstatus/Value:required' => 'erforderlich',
+    'Class:FunctionalCI/Attribute:monitoringstatus/Value:required+' => 'Eine Überwachung ist erforderlich',
+    'Class:FunctionalCI/Attribute:monitoringstatus/Value:unnecessary' => 'unnötig',
+    'Class:FunctionalCI/Attribute:monitoringstatus/Value:unnecessary+' => 'Eine Überwachung ist nicht erforderlich',
+    'Class:FunctionalCI/Attribute:monitoringparameter' => 'Monitoring Parameter',
+    'Class:FunctionalCI/Attribute:monitoringparameter+' => 'Parameter zur Verwendung innerhalb der Monitoring-Software',
+    'Class:FunctionalCI/Attribute:monitoringcore_id' => 'Monitoring Core',
+    'Class:FunctionalCI/Attribute:monitoringcore_id+' => 'Der Core-Server, dem dieses Gerät zugewiesen ist',
+    'Class:FunctionalCI/Attribute:monitoringcore_id_friendlyname' => 'Monitoring Core',
+    'Class:FunctionalCI/Attribute:monitoringprobe_id' => 'Monitoring Probe',
+    'Class:FunctionalCI/Attribute:monitoringprobe_id+' => 'Der Probe-Server, dem dieses Gerät zugewiesen ist',
+    'Class:FunctionalCI/Attribute:monitoringprobe_id_friendlyname' => 'Monitoring Probe',
+    'Class:FunctionalCI/Attribute:monitoringip_id' => 'Monitoring IP',
+    'Class:FunctionalCI/Attribute:monitoringip_id+' => 'IP-Adresse, die zur Überwachung dieses CIs verwendet werden soll (muss den Status \'Zugewiesen\' haben.',
+    'Class:FunctionalCI/Attribute:monitoringip_id_friendlyname' => 'Monitoring IP',
+    'Class:NetworkInterface/Attribute:monitoringstatus' => 'Monitoring Status',
+    'Class:NetworkInterface/Attribute:monitoringstatus+' => 'Status der Implementierung der Überwachung',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:unknown' => 'unbekannt',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:unknown+' => 'unbekannt',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:implemented' => 'implementiert',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:implemented+' => 'Eine Überwachung ist implementiert',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:required' => 'erforderlich',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:required+' => 'Eine Überwachung ist erforderlich',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:unnecessary' => 'unnötig',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:unnecessary+' => 'Eine Überwachung ist nicht erforderlich',
+    'Class:NetworkInterface/Attribute:monitoringparameter' => 'Monitoring Parameter',
+    'Class:NetworkInterface/Attribute:monitoringparameter+' => 'Parameter zur Verwendung innerhalb der Monitoring-Software',
+    'Class:MonitoringDevice' => 'Monitoring Gerät',
+    'Class:MonitoringDevice+' => 'Ein Gerät, dass dazu verwendet wird, andere Geräte zu überwachen',
+    'Class:MonitoringDevice/Attribute:functionalci_id' => 'Monitoring Server',
+    'Class:MonitoringCore' => 'Monitoring Core',
+    'Class:MonitoringCore+' => 'Der Core-Server zur Überwachung anderer Geräte',
+    'Class:MonitoringCore/Attribute:monitoringprobe_list' => 'Monitoring Probes',
+    'Class:MonitoringCore/Attribute:functionalci_list' => 'FunctionalCI Liste',
+    'Class:MonitoringProbe' => 'Monitoring Probe',
+    'Class:MonitoringProbe+' => 'Der Probe-Server zur Überwachung anderer Geräte',
+    'Class:MonitoringProbe/Attribute:functionalci_list' => 'FunctionalCI Liste',
+    'Menu:ConfigManagement:Monitoring' => 'Systemüberwachung',
+    'Menu:ConfigManagement:Monitoring+' => 'Systemüberwachung',
+    'Server:monitoring' => 'Systemüberwachung',
+    'Class:NetworkInterface/Attribute:monitoringstatus' => 'Monitoring Status',
+    'Class:NetworkInterface/Attribute:monitoringstatus+' => 'Status der Implementierung der Überwachung',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:unknown' => 'unbekannt',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:unknown+' => 'unbekannt',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:implemented' => 'implementiert',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:implemented+' => 'Eine Überwachung ist implementiert',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:required' => 'erforderlich',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:required+' => 'Eine Überwachung ist erforderlich',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:unnecessary' => 'unnötig',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:unnecessary+' => 'Eine Überwachung ist nicht erforderlich',
+    'Class:NetworkInterface/Attribute:monitoringparameter' => 'Monitoring Parameter',
+    'Class:NetworkInterface/Attribute:monitoringparameter+' => 'Parameter zur Verwendung innerhalb der Monitoring-Software',
+));
---
+++ en.dict.br-monitoring.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * @copyright   Copyright (C) 2021-2024 Björn Rudner
+ * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
+ * @version     2024-05-24
+ *
+ * Localized data
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+    // Dictionary entries go here
+    'Class:FunctionalCI/Attribute:monitoringstatus' => 'Monitoring Status',
+    'Class:FunctionalCI/Attribute:monitoringstatus+' => 'Status of the monitoring implementation',
+    'Class:FunctionalCI/Attribute:monitoringstatus/Value:unknown' => 'unknown',
+    'Class:FunctionalCI/Attribute:monitoringstatus/Value:unknown+' => 'Implementation status is unknown',
+    'Class:FunctionalCI/Attribute:monitoringstatus/Value:implemented' => 'implemented',
+    'Class:FunctionalCI/Attribute:monitoringstatus/Value:implemented+' => 'Monitoring is implemented',
+    'Class:FunctionalCI/Attribute:monitoringstatus/Value:required' => 'required',
+    'Class:FunctionalCI/Attribute:monitoringstatus/Value:required+' => 'Monitoring is required',
+    'Class:FunctionalCI/Attribute:monitoringstatus/Value:unnecessary' => 'unnecessary',
+    'Class:FunctionalCI/Attribute:monitoringstatus/Value:unnecessary+' => 'Monitoring is not necessary',
+    'Class:FunctionalCI/Attribute:monitoringparameter' => 'Monitoring Parameter',
+    'Class:FunctionalCI/Attribute:monitoringparameter+' => 'Parameters to be used within the monitoring application',
+    'Class:FunctionalCI/Attribute:monitoringcore_id' => 'Monitoring Core',
+    'Class:FunctionalCI/Attribute:monitoringcore_id+' => 'The monitoring core server this device is applied to',
+    'Class:FunctionalCI/Attribute:monitoringcore_id_friendlyname' => 'Monitoring Core',
+    'Class:FunctionalCI/Attribute:monitoringprobe_id' => 'Monitoring Probe',
+    'Class:FunctionalCI/Attribute:monitoringprobe_id+' => 'The monitoring probe server this device is applied to',
+    'Class:FunctionalCI/Attribute:monitoringprobe_id_friendlyname' => 'Monitoring Probe',
+    'Class:FunctionalCI/Attribute:monitoringip_id' => 'Monitoring IP',
+    'Class:FunctionalCI/Attribute:monitoringip_id+' => 'The IP address of this CI to be used for monitoring (must be of status \'assigned\').',
+    'Class:FunctionalCI/Attribute:monitoringip_id_friendlyname' => 'Monitoring IP',
+    'Class:NetworkInterface/Attribute:monitoringstatus' => 'Monitoring status',
+    'Class:NetworkInterface/Attribute:monitoringstatus+' => 'Status of the monitoring implementation',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:unknown' => 'unknown',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:unknown+' => 'Implementation status is unknown',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:implemented' => 'implemented',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:implemented+' => 'Monitoring is implemented',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:required' => 'required',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:required+' => 'Monitoring is required',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:unnecessary' => 'unnecessary',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:unnecessary+' => 'Monitoring is not necessary',
+    'Class:NetworkInterface/Attribute:monitoringparameter' => 'Monitoring Parameter',
+    'Class:NetworkInterface/Attribute:monitoringparameter+' => 'Parameters to be used within the monitoring application',
+    'Class:MonitoringDevice' => 'Monitoring Device',
+    'Class:MonitoringDevice+' => 'The device (core or probe server) used to monitor other devices',
+    'Class:MonitoringDevice/Attribute:functionalci_id' => 'Monitoring Server',
+    'Class:MonitoringCore' => 'Monitoring Core',
+    'Class:MonitoringCore+' => 'The core server used to monitor other devices',
+    'Class:MonitoringCore/Attribute:functionalci_list' => 'FunctionalCI list',
+    'Class:MonitoringCore/Attribute:monitoringprobe_list' => 'Monitoring Probes',
+    'Class:MonitoringProbe' => 'Monitoring Probe',
+    'Class:MonitoringProbe+' => 'The probe server used to monitor other devices',
+    'Class:MonitoringProbe/Attribute:functionalci_list' => 'FunctionalCI list',
+    'Menu:ConfigManagement:Monitoring' => 'Monitoring',
+    'Menu:ConfigManagement:Monitoring+' => 'Systems monitoring',
+    'Server:monitoring' => 'Monitoring',
+    'Class:NetworkInterface/Attribute:monitoringstatus' => 'Monitoring status',
+    'Class:NetworkInterface/Attribute:monitoringstatus+' => 'Status of the monitoring implementation',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:unknown' => 'unknown',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:unknown+' => 'Implementation status is unknown',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:implemented' => 'implemented',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:implemented+' => 'Monitoring is implemented',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:required' => 'required',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:required+' => 'Monitoring is required',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:unnecessary' => 'unnecessary',
+    'Class:NetworkInterface/Attribute:monitoringstatus/Value:unnecessary+' => 'Monitoring is not necessary',
+    'Class:NetworkInterface/Attribute:monitoringparameter' => 'Monitoring Parameter',
+    'Class:NetworkInterface/Attribute:monitoringparameter+' => 'Parameters to be used within the monitoring application',
+));
