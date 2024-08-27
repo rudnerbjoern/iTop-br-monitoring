26e492993dfca1ace9c98da8df91602e595c9a3f
Bjoern Rudner <rudnerbjoern@hotmail.com>
2024-08-27 13:48:02 +0200

    remove obsolete model file

--- module.br-monitoring.php
+++ module.br-monitoring.php
@@ -3,7 +3,7 @@
 /**
  * @copyright   Copyright (C) 2021-2024 Björn Rudner
  * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
- * @version     2024-05-24
+ * @version     2024-08-27
  *
  * iTop module definition file
  */
@@ -29,9 +29,7 @@ SetupWebPage::AddModule(
 
         // Components
         //
-        'datamodel' => array(
-            'model.br-monitoring.php'
-        ),
+        'datamodel' => array(),
         'webservice' => array(),
         'data.struct' => array(
             // add your 'structure' definition XML files here,
